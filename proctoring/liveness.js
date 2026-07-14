80ec11f76b33a68eb3781fc55438e77e7ca1a805
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2026-07-14 14:23:23 +0100

    Create liveness.js

---
+++ proctoring/liveness.js
@@ -0,0 +1,23 @@
+export function calculateEyeAspectRatio(points) {
+
+    return 0;
+}
+
+export function detectBlink() {
+
+    return false;
+}
+
+export function detectSmile() {
+
+    return false;
+}
+
+export function estimateHeadPose() {
+
+    return {
+        yaw: 0,
+        pitch: 0,
+        roll: 0
+    };
+}
